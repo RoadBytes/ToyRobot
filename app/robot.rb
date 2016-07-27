e986361853563fac51e40171a91d536b5994c6fc
Jason Data <jason.data@roadbytes.me>
2016-07-27 09:49:45 -0700

    added place method

--- app/robot.rb
+++ app/robot.rb
@@ -1,5 +1,25 @@
 class Robot
-  def navigate
-    true
+  attr_reader :x, :y, :direction
+  DIRECTIONS = [:NORTH, :SOUTH, :EAST, :WEST]
+  TABLE_SIZE = 5
+
+  def place(x, y, direction)
+    return unless DIRECTIONS.include? direction
+    return unless robot_in_bounds
+    @x         = x
+    @y         = y
+    @direction = direction
+  end
+
+  def report
+    "I'm not on the table"
+  end
+
+  private
+
+  def robot_in_bounds
+    return false if x.nil? or y.nil?
+    x < TABLE_SIZE and y < TABLE_SIZE and
+    x > 0 and y > 0
   end
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -1,9 +1,89 @@
 require_relative "../app/robot.rb"
 
 describe Robot do
-  describe "#navigate" do
-    it "returns true" do
-      expect(Robot.new.navigate).to eq true
+  describe "#report" do
+    it "sends 'not on table' if `place` hasn't been called" do
+      floating_robot = Robot.new
+
+      expect(floating_robot.report).to eq "I'm not on the table"
+    end
+
+    it "sends coordinates from `place` call" do
+      placed_robot = Robot.new
+      placed_robot.place(0, 0, :NORTH)
+
+      expect(placed_robot.report).to eq "0, 1, NORTH"
+    end
+  end
+
+  describe "#place" do
+    context "with valid input" do
+      it "sets x, y, and direction" do
+        robot = Robot.new
+        robot.place(0, 1, :NORTH)
+
+        expect(robot.x).to eq 0
+        expect(robot.y).to eq 1
+        expect(robot.direction).to eq :NORTH
+      end
+    end
+
+    context "with invalid input" do
+      it "ignores commands with non-direction inputs" do
+        robot = Robot.new
+        robot.place(0, 1, :SANDWHICH)
+
+        expect(robot.x).to eq nil
+        expect(robot.y).to eq nil
+        expect(robot.direction).to eq nil
+      end
+
+      it "ignores commands with x over bounds" do
+        robot = Robot.new
+        robot.place(7, 0, :NORTH)
+
+        expect(robot.x).to eq nil
+        expect(robot.y).to eq nil
+        expect(robot.direction).to eq nil
+      end
+
+      it "ignores commands with x under bounds" do
+        robot = Robot.new
+        robot.place(-1, 0, :NORTH)
+
+        expect(robot.x).to eq nil
+        expect(robot.y).to eq nil
+        expect(robot.direction).to eq nil
+      end
+
+      it "ignores commands with y over bounds" do
+        robot = Robot.new
+        robot.place(0, 7, :NORTH)
+
+        expect(robot.x).to eq nil
+        expect(robot.y).to eq nil
+        expect(robot.direction).to eq nil
+      end
+
+      it "ignores commands with y under bounds" do
+        robot = Robot.new
+        robot.place(0, -1, :NORTH)
+
+        expect(robot.x).to eq nil
+        expect(robot.y).to eq nil
+        expect(robot.direction).to eq nil
+      end
+
+      it "leaves x, y, and direction unchanged" do
+        robot = Robot.new
+        robot.place(0, 1, :NORTH)
+
+        robot.place(0, 6, :CALI)
+
+        expect(robot.x).to eq nil
+        expect(robot.y).to eq nil
+        expect(robot.direction).to eq nil
+      end
     end
   end
 end
